09b158791c67f000e7c5de85518f1f9b3173a52c
Rbt5710 <24rajat.sharma@gmail.com>
2026-06-14 17:17:31 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+});
